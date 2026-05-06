a33bb1b11926bab8f96fa620ce0d1481733a1f43
vaidehi80 <vyawharevaidehi@gmail.com>
2026-05-06 15:47:36 +0530

    Change API base URL environment variable

--- college-management/frontend/src/api/axios.js
+++ college-management/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+baseURL: `${process.env.REACT_APP_API_URL}/api`,
   withCredentials: true,
 });
 
