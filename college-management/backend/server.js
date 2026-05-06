02d80013b75f75faef285dc881695186c66fc2c6
vaidehi80 <vyawharevaidehi@gmail.com>
2026-05-06 16:02:40 +0530

    Update server.js

--- college-management/backend/server.js
+++ college-management/backend/server.js
@@ -10,7 +10,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://dev.lkcwsc.vnssorg.com',
+    'https://lkcwsc.vnssorg.com'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
