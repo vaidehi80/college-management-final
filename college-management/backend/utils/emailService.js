85d0c2af7314851096bc99d05a3cd23829b03322
vaidehi80 <vyawharevaidehi@gmail.com>
2026-05-11 17:10:52 +0530

    Update emailService.js

--- college-management/backend/utils/emailService.js
+++ college-management/backend/utils/emailService.js
@@ -1,12 +1,11 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
 });
 
 exports.sendOTPEmail = async (toEmail, otp, userName = 'User') => {
